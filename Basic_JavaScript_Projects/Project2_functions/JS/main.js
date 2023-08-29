c781018f2b3f32bef94014453ea5e4db101d7a25
Abdullah881 <abdullah.altaf881@gmail.com>
2023-08-29 16:57:41 +0500

    Update main.js

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -2,6 +2,11 @@
 function First_Function() {
     var str = " This is Button Text -";
     var name_1 = "Steve";
+    name_1 += " Mike"
     document.getElementById("Demo").innerHTML = str + name_1;
 }
 
+function Add_Function(){
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
