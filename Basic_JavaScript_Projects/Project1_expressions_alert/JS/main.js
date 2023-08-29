0c671da24f157b5402fa9e5f8f2bb97650bca729
Abdullah881 <abdullah.altaf881@gmail.com>
2023-08-29 14:29:07 +0500

    Update main.js

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,18 @@
-var A = " This is a String"
-document.write(A)
+var A = "This is a String"; 
+document.write(A); // Declaring a variable and printing it
 
-window.alert(A)
+document.write('\<br> isn\'t it fantastic' ); // print
+
+
+document.write( "\<br>\" Be who you are and say what you feel," + 
+   " \<br> because those who mind don\'t matter" + 
+   "\<br> and those who matter don\'t mind.\" " + "Dr. Seuss");
+   // print a quote
+
+var B =  "\<br>This is a " + "Test";
+document.write(B);
+// Declaring a variable which is a concatenation of two words and printing it
+
+var student_1 = "Adam" , student_2 = "Paul" , student_2 = "Jake"; // declaring multiple variables
+
+document.write( 3 + 3); // solving and printing an expression 
