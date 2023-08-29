dee184f87e35603aafac56e9fb5645b1d99bfad5
Abdullah881 <abdullah.altaf881@gmail.com>
2023-08-29 17:05:58 +0500

    Update main.js
    Added Divide and Multiplication Function

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -2,10 +2,21 @@
 
 function Add_Function(){
     var addition = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+    document.getElementById("Math1").innerHTML = "2 + 2 = " + addition;
 }
 
 function Sub_Function(){
     var subtract = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2 = " + subtract;
+    document.getElementById("Math2").innerHTML = "5 - 2 = " + subtract;
 }
+
+function Multiply_Function(){
+    var SimpleMath = 6 * 8;
+    document.getElementById("Math3").innerHTML = "6 * 8 = " + subtract;
+}
+
+function Divide_Function(){
+    var SimpleMath = 42/7;
+    document.getElementById("Math4").innerHTML = "42/7 = " + subtract;
+}
+
