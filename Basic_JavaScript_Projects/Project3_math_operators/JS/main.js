3251b3460fac301670bb3978cf89ba96ef1e8664
Abdullah881 <abdullah.altaf881@gmail.com>
2023-08-29 16:59:12 +0500

    Update main.js
    Added Subtract Function

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -4,3 +4,8 @@ function Add_Function(){
     var addition = 2 + 2;
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
+
+function Sub_Function(){
+    var subtract = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + subtract;
+}
