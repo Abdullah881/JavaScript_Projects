bc2c6ed4dbb4f635901d2d57794e11025a7d9a22
Abdullah881 <abdullah.altaf881@gmail.com>
2023-08-30 15:09:17 +0500

    Update
    Display Infinity Assignment

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -4,3 +4,10 @@ document.write(isNaN(213))
 document.write('\<br>')
 document.write(isNaN('33s'))
 document.write('\<br>')
+document.write('<div id="no">2E310</div>')
+document.write('\<br>')
+document.write(-2E310)
+document.write('\<br>')
+document.write(Number((document.getElementById("no").innerHTML)))
+
+
