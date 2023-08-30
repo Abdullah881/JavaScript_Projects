934c7baede95abf55b5c81c2e8622b1708ffe596
Abdullah881 <abdullah.altaf881@gmail.com>
2023-08-30 12:12:30 +0500

    New Assignment
    New Project; Assignment 4

---
+++ Basic_JavaScript_Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,12 @@
+function  my_Dictionary() {
+    var Animal = {
+        species: "Dog",
+        color : "Black",
+        color : "White",
+        Breed : "Husky",
+        age : 5,
+        sound : "Bark"
+    }
+    document.getElementById("Dictionary").innerHTML = Animal.sound;
+
+}
