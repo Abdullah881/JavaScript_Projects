1fd29088c5f32ba8ba340db4c1253a9fc8bc2894
Abdullah881 <abdullah.altaf881@gmail.com>
2023-08-30 15:45:53 +0500

    Update main.js
    use of && , || , == and === Operators

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -15,4 +15,84 @@ document.write('\<br>')
 document.write(-321<231)
 document.write('\<br>')
 console.log(34>231+234)
+document.write('\<br>')
+document.write(-321==231)
+document.write('\<br>')
 
+document.write('\<br>')
+document.write("This section is about testing === sign")
+document.write('\<br>')
+
+
+
+document.write('\<br>')
+document.write("20===20")
+document.write('\<br>')
+document.write(20===20)
+document.write('\<br>')
+
+document.write('\<br>')
+document.write('20 === "steve"')
+document.write('\<br>')
+document.write(20 === "steve")
+document.write('\<br>')
+
+document.write('\<br>')
+document.write('10 === "10"')
+document.write('\<br>')
+document.write(10 === "10")
+document.write('\<br>')
+
+document.write('\<br>')
+document.write("20===21")
+document.write('\<br>')
+document.write(20===21)
+document.write('\<br>')
+
+document.write('\<br>')
+document.write("Use of AND and OR operator")
+document.write('\<br>')
+
+
+
+document.write('\<br>')
+document.write('23>2 AND 32>3')
+document.write('\<br>')
+document.write(23>2 && 32>3)
+document.write('\<br>')
+
+document.write('\<br>')
+document.write('23>2 AND 32>323')
+document.write('\<br>')
+document.write(23>2 && 32>323)
+document.write('\<br>')
+
+
+document.write('\<br>')
+document.write('23>2 OR 32>323')
+document.write('\<br>')
+document.write(23>2 || 32>323)
+document.write('\<br>')
+
+
+
+document.write('\<br>')
+document.write('23>2 OR 32>3')
+document.write('\<br>')
+document.write(23>212 || 32>312)
+document.write('\<br>')
+
+
+document.write('\<br>')
+document.write('NOT (23>2 OR 32>323)')
+document.write('\<br>')
+document.write(!(23>2 || 32>323))
+document.write('\<br>')
+
+
+
+document.write('\<br>')
+document.write('NOT(23>2 OR 32>3)')
+document.write('\<br>')
+document.write(!(23>212 || 32>312))
+document.write('\<br>')
