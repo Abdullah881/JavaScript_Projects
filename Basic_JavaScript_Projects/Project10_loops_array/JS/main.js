0fc51a54799bee40eee0434acfdd82ee235165f3
Abdullah881 <abdullah.altaf881@gmail.com>
2023-09-04 14:19:29 +0500

    New Assignment Created

---
+++ Basic_JavaScript_Projects/Project10_loops_array/JS/main.js
@@ -0,0 +1,27 @@
+// WHILE LOOP
+// function Call_loop(){
+//     var digit = '';
+//     var x = 1;
+//     while (x < 11) {
+//         digit += "<br>" + x;
+//         x++
+
+//     }
+//     document.getElementById("loop").innerHTML = digit;
+
+
+// }
+
+// FOR LOOP
+function Call_loop(){
+    var digit = '';
+    var x = 1;
+    for (i = 0 ; i < 12 ; i++ ) {
+        digit += "<br>" + x;
+        x++
+
+    }
+    document.getElementById("loop").innerHTML = digit;
+
+
+}
